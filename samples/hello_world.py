e6bec22781cfff7b35435835392c895e990a5a55
Konrad Michalik <konrad.michalik@xima.de>
2024-02-19 17:40:20 +0100

    fix: add libs and fonts

--- samples/hello_world.py
+++ samples/hello_world.py
@@ -5,6 +5,7 @@ import os
 import sys
 import logging
 import time
+fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'font')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
@@ -15,19 +16,28 @@ from PIL import Image,ImageDraw,ImageFont
 
 logging.basicConfig(level=logging.DEBUG)
 
-logging.info("hello world")
-epd = epd2in13_V3.EPD()
+try:
+    logging.info("hello world")
+    epd = epd2in13_V3.EPD()
 
-image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-draw = ImageDraw.Draw(image)
+    font15 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 15)
+    font24 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 24)
 
-draw.text((120, 60), 'Just a simple hello world example')
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(image)
 
-time.sleep(5)
+    draw.text((120, 10), 'Just a simple hello world example ...', font = font15, fill = 0)
+    epd.init()
+    epd.display(epd.getbuffer(image))
 
-logging.info("Clear...")
-epd.init()
-epd.Clear(0xFF)
+    time.sleep(5)
 
-logging.info("Goto Sleep...")
-epd.sleep()
+    logging.info("Clear...")
+    epd.init()
+    epd.Clear(0xFF)
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+
+except IOError as e:
+    logging.info(e)
