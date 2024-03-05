4ec33b39447b24bd2967f9f155646dd026dbc61f
Konrad Michalik <konrad.michalik@xima.de>
2024-03-05 21:45:44 +0100

    feat: add init script for easy installation

---
+++ init.sh
@@ -0,0 +1,8 @@
+# https://www.waveshare.com/wiki/7.5inch_e-Paper_HAT_Manual#Python
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
+sudo apt install python3-gpiozero
